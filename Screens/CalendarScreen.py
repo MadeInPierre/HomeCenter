3d8eccd36325402eac2955621a7f2c4909da58bf
Pierre Laclau <pielaclau@gmail.com>
2016-02-24 22:57:23 +0100

    Ajout du fichier CalendarScreen.py
    Création du fichier et écriture de la structure de base.

---
+++ Screens/CalendarScreen.py
@@ -0,0 +1,18 @@
+import pygame, math
+from InputManager import *
+from AnimationManager import *
+
+
+class CalendarScreen():
+
+    def __init__(self, windowres):
+        pass
+
+    def Update(self, InputEvents):
+        pass
+
+    def Draw(self, gameDisplay):
+        pass
+
+    def Quit(self):
+        pass
